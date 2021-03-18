6971cde5b3938e182ce1841ec7a07a3af9c28c35
Michael Taylor <michael.a.taylor@uconn.edu>
2021-03-18 15:34:48 -0400

    create shell endpoint

--- app.py
+++ app.py
@@ -10,17 +10,13 @@ digits = datasets.load_digits()
 @app.route('/')
 @cross_origin()
 def hello():
-    return 'Hello, World!'
+    return 'MLMB Backend'
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
-    clf = svm.SVC(gamma=0.001, C=100.)
-    clf.fit(digits.data[:-1], digits.target[:-1])
-    prediction = clf.predict(digits.data[-1:])
-
     req_data = request.get_json()
-    print(req_data)
-    req_data['predict'] = repr(prediction)
+    req_data['predict_home'] = 'W'
+    req_data['predict_away'] = 'L'
 
     return req_data
